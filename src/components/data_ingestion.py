de86393e28f995e225a660c99de405262d3f2cdc
Dipa <diyapa123.mec@gmail.com>
2025-02-20 21:31:00 +0530

    final_code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -20,6 +20,7 @@ class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
+    original_path: str=os.path.join('artifacts','stud.csv' )
 
 class DataIngestion:
     def __init__(self):
@@ -28,7 +29,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('./notebook/data/stud.csv')
+            df=pd.read_csv(self.ingestion_config.original_path)
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -54,10 +55,10 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
